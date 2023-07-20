4eaa3367100b26ee8434d20998f17a3a2e12bcf1
solomon8392 <120738035+solomon8392@users.noreply.github.com>
2023-07-20 01:08:54 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+// import Sidebar from './Componects/SideBar Section/Sidebar'
+// import Body from './Componects/Body Section/Body'
+
+
+function App() {
+  return (
+    <div className="Container">
+        <h1 className="text-5xl text-[red] flex justify-center ">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
